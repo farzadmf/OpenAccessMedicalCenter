55bb212257027837981bc3ead4c5c5a11510eddf
Farzad Majidfayyaz <farzadmf@hotmail.com>
2015-08-18 14:45:46 -0400

    More Ebean classes

---
+++ app/models/RoomSupply.java
@@ -0,0 +1,46 @@
+package models;
+
+import play.db.ebean.Model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "Room_Supply")
+public class RoomSupply extends Model {
+
+    @Embeddable
+    public class RoomSupplyKey {
+        @Column(name = "Location_ID")
+        public Integer locationId;
+        @Column(name = "Supplies_Internal_Supply_ID")
+        public Integer supplyId;
+
+        @Override
+        public int hashCode() {
+            return locationId.hashCode() + supplyId.hashCode();
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (obj == null)
+                return false;
+
+            if (!(obj instanceof RoomSupplyKey))
+                return false;
+
+            RoomSupplyKey other = (RoomSupplyKey) obj;
+            return (other.locationId.equals(locationId) && other.supplyId.equals(supplyId));
+        }
+    }
+
+    @EmbeddedId
+    private RoomSupplyKey key;
+
+    @Column(name = "Quantity")
+    private Integer quantity;
+
+    @Column(name = "Alotment")
+    private Integer allotment;
+
+    //TODO: Add foreign keys for "Location" and "Supply"
+}
