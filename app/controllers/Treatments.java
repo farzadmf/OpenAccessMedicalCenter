bf935356909bd063c57867d2c0b1ed2ffe348240
Farzad Majidfayyaz <farzadmf@hotmail.com>
2015-08-20 16:43:40 -0400

    Finish the update functionality for treatment scheduling

--- app/controllers/Treatments.java
+++ app/controllers/Treatments.java
@@ -2,11 +2,8 @@ package controllers;
 
 import com.avaje.ebean.Ebean;
 import helpers.Security.Authorized;
-import models.current.Employee;
+import models.current.*;
 import models.current.EmployeeType.EmployeeTypes;
-import models.current.Patient;
-import models.current.PatientTreatment;
-import models.current.Treatment;
 import play.data.Form;
 import play.mvc.Controller;
 import play.mvc.Result;
@@ -22,15 +19,16 @@ public class Treatments extends Controller {
     private static List<Patient> patients = Ebean.find(Patient.class).findList();
     private static List<Employee> employees = Ebean.find(Employee.class).findList();
     private static List<Treatment> treatments = Ebean.find(Treatment.class).findList();
+    private static List<Location> locations = Ebean.find(Location.class).findList();
 
-    @Authorized(role = EmployeeTypes.Administrator)
     public static Result create() {
         Form<Object> surgeryForm = Form.form(Object.class);
         surgeryForm.data().put("patientTreatmentId", "0");
         surgeryForm.data().put("patientId", "0");
         surgeryForm.data().put("employeeId", "0");
         surgeryForm.data().put("treatmentId", "0");
-        return ok(treatmentSchedule.render(surgeryForm, patients, employees, treatments));
+        surgeryForm.data().put("locationId", "0");
+        return ok(treatmentSchedule.render(surgeryForm, patients, employees, treatments, locations));
     }
 
     public static Result save() {
@@ -39,11 +37,13 @@ public class Treatments extends Controller {
         int patientId = Integer.parseInt(surgeryForm.data().get("patient"));
         int employeeId = Integer.parseInt(surgeryForm.data().get("employee"));
         int treatmentId = Integer.parseInt(surgeryForm.data().get("treatment"));
-        String date = surgeryForm.data().get("date");
+        int locationId = Integer.parseInt(surgeryForm.data().get("location"));
+//        String date = surgeryForm.data().get("date");
 
         Patient patient = Ebean.find(Patient.class, patientId);
         Employee employee = Ebean.find(Employee.class, employeeId);
         Treatment treatment = Ebean.find(Treatment.class, treatmentId);
+        Location location = Ebean.find(Location.class, locationId);
 
         PatientTreatment patientTreatment;
 
@@ -55,13 +55,15 @@ public class Treatments extends Controller {
         patientTreatment.patient = patient;
         patientTreatment.employee = employee;
         patientTreatment.treatment = treatment;
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
-        simpleDateFormat.setLenient(false);
-        try {
-            patientTreatment.date = (Date) simpleDateFormat.parse(date);
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
+        patientTreatment.location = location;
+//        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+//        simpleDateFormat.setLenient(false);
+//        try {
+//            Date newDate = new Date(simpleDateFormat.parse(date).getDate());
+//            patientTreatment.date = newDate;
+//        } catch (ParseException e) {
+//            e.printStackTrace();
+//        }
 
         Ebean.save(patientTreatment);
         flash("success", "Saved Treatment with ID " + patientTreatment.patientTreatmentId);
@@ -76,7 +78,8 @@ public class Treatments extends Controller {
         surgeryForm.data().put("patientId", treatment.patient.patientId.toString());
         surgeryForm.data().put("employeeId", treatment.employee.employeeId.toString());
         surgeryForm.data().put("treatmentId", treatment.treatment.treatmentId.toString());
-        surgeryForm.data().put("date", treatment.date.toString());
-        return ok(treatmentSchedule.render(surgeryForm, patients, employees, treatments));
+        surgeryForm.data().put("locationId", treatment.location.locationId.toString());
+//        surgeryForm.data().put("date", treatment.date.toString());
+        return ok(treatmentSchedule.render(surgeryForm, patients, employees, treatments, locations));
     }
 }
